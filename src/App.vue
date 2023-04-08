bc100b428f089caea5671d7da2d71fbb91e27d1b
Javier Sosa Breyer <13318348+SosaBreyer@users.noreply.github.com>
2023-04-08 02:12:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script>
+  import EmployeeForm from './components/EmployeeForm.vue'
+  import EmployeeTable from './components/EmployeeTable.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      EmployeeForm,
+      EmployeeTable,
+    },
+    data() {
+      return {
+        employees: [
+          {
+            id: 1,
+            name: 'Richard Hendricks',
+            email: 'richard@piedpiper.com',
+          },
+          {
+            id: 2,
+            name: 'Bertram Gilfoyle',
+            email: 'gilfoyle@piedpiper.com',
+          },
+          {
+            id: 3,
+            name: 'Dinesh Chugtai',
+            email: 'dinesh@piedpiper.com',
+          },
+        ],
+      }
+    },
+    methods: {
+      addEmployee(employee) {
+        const lastId =
+          this.employees.length > 0
+            ? this.employees[this.employees.length - 1].id
+            : 0;
+        const id = lastId + 1;
+        const newEmployee = { ...employee, id };
+
+        this.employees = [...this.employees, newEmployee];
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <EmployeeForm @add:employee="addEmployee" />
+    <EmployeeTable v-bind:employees="employees" />
+  </div>
+</template>
+
+<style scoped>
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
+</style>
